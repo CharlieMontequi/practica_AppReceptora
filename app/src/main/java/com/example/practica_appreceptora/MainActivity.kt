c5680e7d4b0a65db428e43d0cd357b657a84c588
admin <carlos.montequi.rod@gmail.com>
2023-12-11 17:37:30 +0100

    permisos añadidos, el tema de intents mnirar en el manifeste y comprar con otras apps hechas

--- app/src/main/java/com/example/practica_appreceptora/MainActivity.kt
+++ app/src/main/java/com/example/practica_appreceptora/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.practica_appreceptora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mensajeRecivido : TextView = findViewById(R.id.txtV_mostrarMensaje)
     }
 }
